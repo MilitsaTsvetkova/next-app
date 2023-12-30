531fb6e6d6e4105f1aa0cc0e6f639c82e3dfe39e
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2023-12-30 12:37:07 +0200

    add authentication

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,16 +1,26 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 function Navbar() {
+  const { status, data: session } = useSession();
+
   return (
-    <div className="flex bg-slate-200 p-5">
-      <Link href="/" className="mr-5">
-        Home
-      </Link>
-      <Link href="/users" className="mr-5">
-        Users
-      </Link>
+    <div className="flex bg-slate-200 p-5 space-x-3">
+      <Link href="/">Home</Link>
+      <Link href="/users">Users</Link>
       <Link href="/admin">Admin</Link>
+      {status === "authenticated" ? (
+        <div>
+          {session.user!.name}
+          <Link href="/api/auth/signout" className="ml-3">
+            Sign Out
+          </Link>
+        </div>
+      ) : (
+        <Link href="/api/auth/signin">Sign in</Link>
+      )}
     </div>
   );
 }
