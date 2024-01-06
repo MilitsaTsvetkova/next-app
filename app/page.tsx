f18e473d1fdccfee9977582e04571224b9d5537b
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-01-06 14:21:15 +0200

    seo metadata - global + page dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
 import Image from 'next/image'
 import Sandstone from '@/public/images/Sandstone.jpg'
+import { Metadata } from 'next'
 export default async function Home() {
   const session = await getServerSession(authOptions)
   return (
@@ -21,4 +22,12 @@ export default async function Home() {
       /> */}
     </main>
   )
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const product: any = await fetch('')
+  return {
+    title: product.title,
+    description: product.description,
+  }
 }
