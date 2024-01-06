75a22f58bb37997c5dc6a02a22134b92159f26f1
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-01-06 13:15:14 +0200

    optimization techniques images

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-
+import ProductCard from './components/ProductCard/ProductCard'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import Image from 'next/image'
+import Sandstone from '@/public/images/Sandstone.jpg'
 export default async function Home() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
   return (
-    <main>
+    <main className='relative h-screen'>
       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <ProductCard />
+      <Image src={Sandstone} width={200} height={200} alt='Sandstone plateau' />
+      <Image
+        src='https://images.unsplash.com/photo-1682687220067-dced9a881b56?q=80&w=2875&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        fill
+        sizes='(max-width: 480px) 100vw, (max-width:768px) 50vw, 33vw'
+        className='object-cover'
+        alt='Sandstone plateau'
+        quality={75}
+        priority
+      />
     </main>
-  );
+  )
 }
